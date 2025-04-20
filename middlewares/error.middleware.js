855ecbe790429b1c61f90b464b9f8f4fb938dc5d
Grxson <gael.grxson@gmail.com>
2025-04-20 17:09:07 -0600

    configuración de errores

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,9 @@
+import ApiError from "../utils/apiError.js";
+
+export const errorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        status: "error",
+        message: err.message || "Internal Server Error",
+    });
+}
